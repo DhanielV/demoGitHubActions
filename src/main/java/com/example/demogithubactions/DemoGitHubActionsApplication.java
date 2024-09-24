c31868ddbc08de220c95284928420ba0143d2750
Daniel Vive <dafi0001@stud.kea.dk>
2024-09-24 13:35:05 +0200

    initial commit

---
+++ src/main/java/com/example/demogithubactions/DemoGitHubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitHubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGitHubActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demogithubactions/DemoGitHubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demogithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitHubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
